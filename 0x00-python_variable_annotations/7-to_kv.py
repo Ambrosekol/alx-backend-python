32dc825ecc0d3d4348650ac13835f05ad00fdb42
ambrose <kolus7381@gmail.com>
2024-01-11 19:20:02 +0100

    to key val

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Write a type-annotated function to_kv that takes a string k
+and an int OR float v as arguments and returns a tuple.
+The first element of the tuple is the string k. The second
+element is the square of the int/float v and should be
+annotated as a float.
+"""
+
+
+from typing import Tuple
+
+
+def to_kv(k: str, v: int | float) -> Tuple[str, int | float]:
+    """
+    Write a type-annotated function to_kv that takes a string k
+    and an int OR float v as arguments and returns a tuple.
+    The first element of the tuple is the string k. The second
+    element is the square of the int/float v and should be
+    annotated as a float.
+    """
+    sqare: int | float = v*v
+    return (k, sqare)
